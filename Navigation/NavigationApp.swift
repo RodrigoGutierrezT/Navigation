a14453f6b6e3ae97421f8fffd1f1a44d2f6c7fe3
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-18 23:56:57 -0400

    initial commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Rodrigo on 18-07-24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
