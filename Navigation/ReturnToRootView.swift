282aaf40602a846aad726d547e799e378efba583
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-21 23:12:26 -0400

    feat: Add save path functionality using PathStore

--- Navigation/ReturnToRootView.swift
+++ Navigation/ReturnToRootView.swift
@@ -7,16 +7,49 @@
 
 import SwiftUI
 
-struct DetailView: View {
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
     
-    @Binding var path: NavigationPath
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("failed to save navigation data")
+        }
+        
+    }
+}
+
+struct DetailView: View {
     let number: Int
+    @Binding var path: NavigationPath
     
     var body: some View {
         NavigationLink("Go to Number", value: Int.random(in: 1...1000))
             .navigationTitle("Number: \(number)")
             .toolbar {
-                Button("Go Home") {
+                Button("Home") {
                     path = NavigationPath()
                 }
             }
@@ -24,13 +57,13 @@ struct DetailView: View {
 }
 
 struct ReturnToRootView: View {
-    @State private var path = NavigationPath()
+    @State private var pathStore = PathStore()
     
     var body: some View {
-        NavigationStack(path: $path) {
-            DetailView(path: $path, number: 0)
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0, path: $pathStore.path)
                 .navigationDestination(for: Int.self) { i in
-                    DetailView(path: $path, number: i)
+                    DetailView(number: i, path: $pathStore.path)
                 }
         }
     }
